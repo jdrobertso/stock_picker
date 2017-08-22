7fce86cb9f19179bcadd9dd42eb87e92ddac598f
Jeremy <jrobertson.88@gmail.com>
2017-08-22 00:01:49 -0500

    Added logic to complete the assignment

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,18 @@
 def stock_picker(prices)
-	lowest = prices.m
+	profit = 0
+
+	for x in 0...prices.length
+		for y in (x+1)...prices.length
+			if (prices[y] - prices[x] > profit)
+				profit = prices[y] - prices[x]
+				day_to_buy = x
+				day_to_sell = y
+			end
+		end
+	end
+	array = [day_to_buy, day_to_sell]
+	puts array
+	return array
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
